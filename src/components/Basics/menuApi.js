cf63970bacb4991bc0a0eaeca0c6ce220e236ad2
rajatgit121 <rajjt745@gmail.com>
2023-09-24 13:39:25 +0530

    upload restarant project

---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,115 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Aloo Pakora",
+      category: "evening",
+      price: "20₹",
+      description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes.",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "It is a very good choice for breakfast. Simply boil the sweetcorn, throw some veggies and have it for breakfast or make some simple recipes with boiled sweetcorn to fill your day with energy.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "Punjabi Chola Masala",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Punjabi chole masala or chana masala is one of the most popular curry dishes from India. In fact in north India no wedding or party is complete until you have chole masala on the menu. I remember whenever we would call someone over for lunch or dinner, mom would almost always make chole. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Masala is a thick and creamy red kidney beans curry in onion tomato and base flavored with spices. Here is how to make Punjabi style . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "../images/vegthali.jpg",
+      name: "Veg Thali",
+      category: "dinner",
+      price: "100₹",
+      description:
+        "Variety of Indian thali is one of the my favourite Indian restaurant and I ordered yellow Dal and jeera aalu from this outlet the food was basic and simple and tasty    it was purely good in taste the ingredients were really lite and good just like a home-cooked food.",
+    },
+    {
+      id: 11,
+      image: "../images/aloopratha.jpg",
+      name: "Aloo Paratha",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Potato paratha with detailed photo and video recipe. a classical north indian stuffed bread recipe made with wheat flour and spiced potato mash for stuffing. it is one of the popular paratha recipes which is not only easy to make but also one of the tasty stuffed bread from the indian cuisine. it is generally served for lunch and dinner with a choice of raita and pickle but can also be served for morning breakfast and brunch.",
+    },
+    {
+      id: 12,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+   
+  ];
+  
+  export default Menu;
+  
